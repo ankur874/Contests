8cffc7ba1de10a10f91eac48205038a74608cee9
Ankur Attri <ankurattri874@gmail.com>
2021-06-08 16:30:02 +0530

    done

--- Leetcode/5th June Contest/Leecode1889.cpp
+++ Leetcode/5th June Contest/Leecode1889.cpp
@@ -1,33 +1,39 @@
 #include<vector>
 template <typename T>
+
 class Solution {
+typedef long long ll;
+ll mod=1e9+7;
 public:
     int minWastedSpace(vector<int>& packages, vector<vector<int>>& boxes) {
          sort(packages.begin(),packages.end());
          for(int i=0;i<boxes.size();i++){
              sort(boxes[i].begin(),boxes[i].end());
          }
-         int minWasted=INT_MAX;
+         ll sum=0;
+         for(int i=0;i<packages.size();i++){
+           sum+=packages[i];
+         }
+         ll minWasted=LONG_MAX;
          bool isPresent=false;
          for(int i=0;i<boxes.size();i++){
-             int minWastedInI=0;
-             int p=0;
-             int b=0;
              if(packages[packages.size()-1]<=boxes[i][boxes[i].size()-1]){
+             ll minWastedInI=0;
+             int b=0;
              isPresent=true;
-             while(p<packages.size()&&b<boxes[i].size()){
-                 if(boxes[i][b]>=packages[p]){
-                     minWastedInI+=(boxes[i][b]-packages[p]);
-                     p++;
-                 }else{
-                     b++;
-                 }
+             int currI;
+             int prev=0;
+             while(b<boxes[i].size()){
+                 currI=upper_bound(packages.begin()+prev,packages.end(),boxes[i][b])-packages.begin();
+                 minWastedInI=minWastedInI+boxes[i][b]*(currI-prev);
+                 prev=currI;
+                 b++;
              }
-             minWasted=min(minWasted,minWastedInI);
+             minWasted=min(minWasted,minWastedInI)%mod;
              }
          }
       if(isPresent==true){
-          return minWasted;
+          return (minWasted-sum)%mod;
       }
       return -1;
          
