0991f4065899de3f4a2aaafc0ca4b8881fdd82c0
Ankur Attri <ankurattri874@gmail.com>
2021-06-20 09:44:14 +0530

    contest

---
+++ Leetcode/20th June Contest/5788.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<string>
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        if(num==""){
+            return "";
+        }
+        string ans="";
+         for(int i=num.length()-1;i>=0;i--){
+               if((num[i]-'0')&(1<<0)){
+                   ans=num.substr(0,i+1);
+                   break;
+               }
+         }
+         return ans;
+    }
+};
