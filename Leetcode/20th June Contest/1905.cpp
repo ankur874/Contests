74dc7daf2f9fc4614aaf8688a3a0ba17d9b20f32
Ankur Attri <ankurattri874@gmail.com>
2021-06-21 21:28:14 +0530

    3rd

--- Leetcode/20th June Contest/1905.cpp
+++ Leetcode/20th June Contest/1905.cpp
@@ -1,7 +1,32 @@
 template <typename t>
 class Solution {
 public:
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int &flag,int n,int m,int i,int j){
+        if(i<0||j<0||i>=n||j>=m||grid2[i][j]==0)return ;
+        if(grid1[i][j]==0)flag=0;
+        grid2[i][j]=0;
+        dfs(grid1,grid2,flag,n,m,i,j-1);
+        dfs(grid1,grid2,flag,n,m,i,j+1);
+        dfs(grid1,grid2,flag,n,m,i-1,j);
+        dfs(grid1,grid2,flag,n,m,i+1,j);
+
+
+
+    }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         
+        int n=grid1.size();
+        int m=grid1[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+              if(grid2[i][j]==1){
+                  int flag=1;
+              dfs(grid1,grid2,flag,n,m,i,j);
+              ans+=flag;
+              }
+            }
+        }
+       return ans;
     }
 };
--- Leetcode/20th June Contest/5790.cpp
+++ Leetcode/20th June Contest/5790.cpp
@@ -4,22 +4,6 @@ class Solution {
     
 public:
     vector<int> minDifference(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<int>ans;
-          for(int i=0;i<queries.size();i++){
-              int l=queries[i][0];
-              int r=queries[i][1];
-              int mini=INT_MAX;
-              for(int j=l;j<r;j++){
-                  if(abs(nums[j]-nums[j+1])!=0)
-                  mini=min(mini,abs(nums[j]-nums[j+1]));
-              }
-              if(mini==0)
-                  ans.push_back(-1);
-              else
-              ans.push_back(mini);
-          }    
-          return ans;
-
                  
     }
 };
